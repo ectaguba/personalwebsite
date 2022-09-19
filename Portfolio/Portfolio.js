00d03dc52616cebe2c8b77e9583cb434973ec3a7
niichirin <itschristaguba@gmail.com>
2022-09-19 11:09:57 -0700

    Adjusted video background color in montageFunction();
    gi

--- Portfolio/Portfolio.js
+++ Portfolio/Portfolio.js
@@ -10,10 +10,10 @@ function montageFunction() {
     if (video.paused) {
       aboutMeContainer.style.backgroundColor = "rgba(21, 21, 21, 0)";
       setTimeout(() => { video.play(); }, 0250);
-      montageBtn.innerHTML = "Pause";
+      montageBtn.innerHTML = "Pause Video";
     } else {
-      aboutMeContainer.style.backgroundColor = "rgba(21, 21, 21, 0.8)"
+      aboutMeContainer.style.backgroundColor = "rgba(21, 21, 21, 1)"
       setTimeout(() => { video.pause(); }, 0250);
-      montageBtn.innerHTML = "Play";
+      montageBtn.innerHTML = "Play Video";
     }
 }
